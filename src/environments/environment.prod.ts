c0a3f003650befd5f70f32a691d0bae82192971f
olivierlasbleis <lasbleis.olivier29@gmail.com>
2020-09-09 01:41:18 +0200

    backendurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // ajout d'une URL backend en mode production
-  backendUrl: 'https://pokr-api.herokuapp.com/',
+  backendUrl: 'https://pokr-api.herokuapp.com',
 };
