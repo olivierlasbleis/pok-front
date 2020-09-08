22d23b6cc22d8ccffaf3f816c4643c7888037089
olivierlasbleis <lasbleis.olivier29@gmail.com>
2020-09-09 01:28:46 +0200

    backendurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // ajout d'une URL backend en mode production
-  backendUrl: 'https://air-data-france-api.herokuapp.com',
+  backendUrl: 'https://pokr-api.herokuapp.com/',
 };
